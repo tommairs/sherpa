77e8adfa32f0d7379985438199a59f22f90831c1
Tom Mairs <tom.mairs@messagesystems.com>
2022-02-23 10:56:15 -0700

    Update env.php

--- env.php
+++ env.php
@@ -4,7 +4,7 @@
 /** Edit these settings only **/
 
 // if you do not add an API key here it will be stored temporarily in your browser session cache
-// if you DO add on here, make sur eyou add another layer of security to prevent abuse
+// if you DO add on here, make sure you add another layer of security to prevent abuse
 $apikey = "";
 $apidomain = "api.sparkpost.com";
 $TZ = 'America/Edmonton';
